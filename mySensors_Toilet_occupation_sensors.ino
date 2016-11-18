14c08df2dc918b8304394d831773e13f06c06c1f
AlexBGoode <me.at.forum@gmail.com>
2016-11-19 00:38:37 +0300

    Two metrics
    Now with the status and distance

--- mySensors_Toilet_occupation_sensors.ino
+++ mySensors_Toilet_occupation_sensors.ino
@@ -1,61 +1,87 @@
 // Inspired by
 // https://www.mysensors.org/build/distance
 // http://playground.arduino.cc/Code/NewPing
+// http://www.micropik.com/PDF/HCSR04.pdf
 
 // Enable debug prints
 // #define MY_DEBUG
 
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
-//#define MY_RADIO_RFM69
 
 #include <SPI.h>
 #include <MySensors.h>
-#define CHILD_ID 1
+#define CHILD_ID_SEAT     1
+#define CHILD_ID_OBJECT   2
 
-
-//unsigned long SLEEP_TIME = 2000; // Sleep time between reports (in milliseconds)
 #define SLEEP_TIME      500 // Sleep time between reports (in milliseconds)
 
 #include <NewPing.h>
-#define TRIGGER_PIN     4   // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN        3   // Arduino pin tied to echo pin on the ultrasonic sensor.#define CHILD_ID 1   // Id of the sensor child
-#define MAX_DISTANCE    40  // Max distance we want to start detecting (in cm)
-#define OCCUPAED_DISTANCE 20  // Distance when "parked signal" should be sent to controller (in cm)
+#define TRIGGER_PIN       4   // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN          3   // Arduino pin tied to echo pin on the ultrasonic sensor.#define CHILD_ID 1   // Id of the sensor child
+#define DISTANCE_MAX      100 // Max distance we want to start detecting (in cm)
+#define DISTANCE_OCCUPAED 50 // Distance when inverted "occupied signal" should be sent to controller (in cm)
 
-int skipZero = 0;
 int oldOccupiedStatus = -1;
-//unsigned long sendInterval = 5000;  // Send park status at maximum every 5 second.
-//unsigned long lastSend;
 
 // NewPing setup of pins and maximum distance.
-NewPing sonar( TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE );
+NewPing sonar( TRIGGER_PIN, ECHO_PIN, DISTANCE_MAX );
 
 // Initialize occupation message
-MyMessage msg( CHILD_ID, V_TRIPPED );
+MyMessage msgSeat( CHILD_ID_SEAT, V_TRIPPED );
+// Initialize distance message
+MyMessage msgObject( CHILD_ID_OBJECT, V_DISTANCE );
 
-void presentation()  {
+void presentation()
+{
   // Send the sketch version information to the gateway and Controller
-  sendSketchInfo( "Toilet Sensor", "1.0" );
+  sendSketchInfo( "Toilet occupation sensor", "1.0" );
 
   // Register all sensors to gw (they will be created as child devices)
-  present( CHILD_ID, S_DOOR, "Toilet occupation status" );  
+  present( CHILD_ID_SEAT, S_DOOR, "Toilet is vacant" );
+  present( CHILD_ID_OBJECT, S_DISTANCE, "Distance to object" );
 }
 
-void loop() {
-  unsigned int distance = ( sonar.ping_median() / US_ROUNDTRIP_CM );
+void loop()
+{
+  static int lastDist = -1;
+  int distance = ( sonar.ping_median() / US_ROUNDTRIP_CM );
+//  int distance = sonar.ping_cm();
+  if ( distance < 11)
+    return;
+
+  if (( distance != lastDist ) && ( abs( lastDist - distance ) > 5 ))
+  {
+    // Report a distance if changed more than by 10
+    send( msgObject.set( distance ), true );
+    Serial.print( "Distance changed: " );
+    Serial.println( distance );
+    lastDist = distance;
+    Serial.print( "Distance: " );
+    Serial.println( distance );
+  }
 
   // Get the toilet occupation status
-  int occupied = distance != 0 && distance < OCCUPAED_DISTANCE;
+  int occupied = ( distance > 10 ) && ( distance < DISTANCE_OCCUPAED );
 
-  if ( occupied != oldOccupiedStatus ) {
+//  Serial.println( distance != NO_ECHO );
+//  Serial.println( distance < DISTANCE_OCCUPAED );
+
+
+
+  // First time always false and cause reporting the actual status
+  if ( occupied != oldOccupiedStatus )
+  {
     if ( occupied )
-      Serial.println("Toilet occupied");
+      Serial.println( "Toilet occupied" );
     else
-      Serial.println("Toilet vacant");
+      Serial.println( "Toilet vacant" );
+
     // Report an inverted value for a door lock (as a vacant status)
-    send( msg.set( !occupied ));
+    send( msgSeat.set( !occupied ), true );
     oldOccupiedStatus = occupied;
+    // Double sleep time on change to avoid bouncing
+    sleep( 5000 );
   }
 
   sleep( SLEEP_TIME );
